e5ac0072744d7c73580aee221a719bb55ee4437f
Andy Ehrenberg <andyehrenberg@gmail.com>
2021-07-13 15:07:48 +0100

    PLR

--- level_replay/algo/buffer.py
+++ level_replay/algo/buffer.py
@@ -461,6 +461,81 @@ class AtariBuffer(AbstractBuffer):
         self.tree.batch_set(ind, priority)
 
 
+class RolloutStorage(object):
+    def __init__(self, num_steps, num_processes, obs_shape, action_space):
+        self.obs = torch.zeros(num_steps + 1, num_processes, *obs_shape)
+        self.rewards = torch.zeros(num_steps, num_processes, 1)
+        self.value_preds = torch.zeros(num_steps + 1, num_processes, 1)
+        self.returns = torch.zeros(num_steps + 1, num_processes, 1)
+        if action_space.__class__.__name__ == "Discrete":
+            action_shape = 1
+        else:
+            action_shape = action_space.shape[0]
+        self.actions = torch.zeros(num_steps, num_processes, action_shape)
+        if action_space.__class__.__name__ == "Discrete":
+            self.actions = self.actions.long()
+        self.masks = torch.ones(num_steps + 1, num_processes, 1)
+
+        # Masks that indicate whether it's a true terminal state
+        # or time limit end state
+        # self.bad_masks = torch.ones(num_steps + 1, num_processes, 1)
+
+        self.level_seeds = torch.zeros(num_steps, num_processes, 1, dtype=torch.int)
+
+        self.num_steps = num_steps
+        self.step = 0
+
+    def to(self, device):
+        self.obs = self.obs.to(device)
+        self.rewards = self.rewards.to(device)
+        self.value_preds = self.value_preds.to(device)
+        self.returns = self.returns.to(device)
+        self.actions = self.actions.to(device)
+        self.masks = self.masks.to(device)
+        # self.bad_masks = self.bad_masks.to(device)
+        self.level_seeds = self.level_seeds.to(device)
+
+    def insert(
+        self,
+        obs,
+        actions,
+        value_preds,
+        rewards,
+        masks,  # bad_masks,
+        level_seeds=None,
+    ):
+        if len(rewards.shape) == 3:
+            rewards = rewards.squeeze(2)
+        self.obs[self.step + 1].copy_(obs)
+        self.actions[self.step].copy_(actions)
+        self.value_preds[self.step].copy_(value_preds)
+        self.rewards[self.step].copy_(rewards)
+        self.masks[self.step + 1].copy_(masks)
+        # self.bad_masks[self.step + 1].copy_(bad_masks)
+
+        if level_seeds is not None:
+            self.level_seeds[self.step].copy_(level_seeds)
+
+        self.step = (self.step + 1) % self.num_steps
+
+    def after_update(self):
+        self.obs[0].copy_(self.obs[-1])
+        self.masks[0].copy_(self.masks[-1])
+        # self.bad_masks[0].copy_(self.bad_masks[-1])
+
+    def compute_returns(self, next_value, gamma, gae_lambda):
+        self.value_preds[-1] = next_value
+        gae = 0
+        for step in reversed(range(self.rewards.size(0))):
+            delta = (
+                self.rewards[step]
+                + gamma * self.value_preds[step + 1] * self.masks[step + 1]
+                - self.value_preds[step]
+            )
+            gae = delta + gamma * gae_lambda * self.masks[step + 1] * gae
+            self.returns[step] = gae + self.value_preds[step]
+
+
 # class PLRBuffer:
 # def __init__(self, state_dim, batch_size, buffer_size, device, prioritized, seeds, num_updates):
 # self.buffers = {
